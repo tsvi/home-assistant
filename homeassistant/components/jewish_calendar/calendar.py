a8cc82c65ddf9303c8fc2bd4930fb056a889d490
Tsvi Mostovicz <ttmost@gmail.com>
2024-09-16 19:49:55 +0300

    Initial commit Jewish Calendar - calendar entity

---
+++ homeassistant/components/jewish_calendar/calendar.py
@@ -0,0 +1,72 @@
+"""Jewish Calendar calendar platform."""
+
+from datetime import datetime
+from typing import Any
+from homeassistant.components.calendar import CalendarEntity, CalendarEvent
+from homeassistant.components.calendar.const import CalendarEntityFeature
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.entity import EntityDescription
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from .const import DOMAIN
+from .entity import JewishCalendarEntity
+
+CALENDAR_TYPE = EntityDescription(
+    key="calendar",
+    name="Calendar",
+    icon="mdi:calendar",
+)
+
+async def async_setup_entry(
+    hass: HomeAssistant,
+    config_entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback,
+) -> None:
+    """Set up the Jewish Calendar config entry."""
+    entry = hass.data[DOMAIN][config_entry.entry_id]
+    async_add_entities([JewishCalendar(config_entry, entry, CALENDAR_TYPE)])
+
+
+class JewishCalendar(JewishCalendarEntity, CalendarEntity):
+    """Representation of a Jewish Calendar element."""
+
+    _attr_supported_features = (
+        CalendarEntityFeature.CREATE_EVENT
+        | CalendarEntityFeature.DELETE_EVENT
+        | CalendarEntityFeature.UPDATE_EVENT
+    )
+
+    @property
+    def event(self) -> CalendarEvent | None:
+        """Return the next upcoming event."""
+        raise NotImplementedError
+
+    async def async_get_events(
+        self, hass: HomeAssistant, start_date: datetime, end_date: datetime
+    ) -> list[CalendarEvent]:
+        """Return calendar events within a datetime range."""
+        raise NotImplementedError
+
+    async def async_create_event(self, **kwargs: Any) -> None:
+        """Add a new event to calendar."""
+        raise NotImplementedError
+
+    async def async_delete_event(
+        self,
+        uid: str,
+        recurrence_id: str | None = None,
+        recurrence_range: str | None = None,
+    ) -> None:
+        """Delete an event on the calendar."""
+        raise NotImplementedError
+
+    async def async_update_event(
+        self,
+        uid: str,
+        event: dict[str, Any],
+        recurrence_id: str | None = None,
+        recurrence_range: str | None = None,
+    ) -> None:
+        """Delete an event on the calendar."""
+        raise NotImplementedError
