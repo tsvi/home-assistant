c6db0502497a663996c6b50510672e9c2fbc6571
Tsvi Mostovicz <ttmost@gmail.com>
2024-11-01 00:06:16 +0200

    runtime data in entity

--- homeassistant/components/jewish_calendar/entity.py
+++ homeassistant/components/jewish_calendar/entity.py
@@ -7,6 +7,7 @@ from homeassistant.const import CONF_LANGUAGE, CONF_LOCATION
 from homeassistant.helpers.device_registry import DeviceEntryType, DeviceInfo
 from homeassistant.helpers.entity import Entity, EntityDescription
 
+from . import JCalConfigEntry
 from .const import (
     CONF_CANDLE_LIGHT_MINUTES,
     CONF_DIASPORA,
@@ -22,8 +23,7 @@ class JewishCalendarEntity(Entity):
 
     def __init__(
         self,
-        config_entry: ConfigEntry,
-        data: dict[str, Any],
+        config_entry: JCalConfigEntry,
         description: EntityDescription,
     ) -> None:
         """Initialize a Jewish Calendar entity."""
@@ -34,8 +34,9 @@ class JewishCalendarEntity(Entity):
             identifiers={(DOMAIN, config_entry.entry_id)},
             name=config_entry.title,
         )
-        self._location = data[CONF_LOCATION]
-        self._hebrew = data[CONF_LANGUAGE] == "hebrew"
-        self._candle_lighting_offset = data[CONF_CANDLE_LIGHT_MINUTES]
-        self._havdalah_offset = data[CONF_HAVDALAH_OFFSET_MINUTES]
-        self._diaspora = data[CONF_DIASPORA]
+        data = config_entry.runtime_data
+        self._location = data.location
+        self._hebrew = data.language == "hebrew"
+        self._candle_lighting_offset = data.candle_lighting_offset
+        self._havdalah_offset = data.havdalah_offset
+        self._diaspora = data.diaspora
